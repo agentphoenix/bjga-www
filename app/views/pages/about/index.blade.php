deddaafdf9f6120944e190523496e7023b211ea6
David VanScott <david.vanscott@gmail.com>
2012-11-24 16:24:42 -0500

    Tablet tweaks

--- app/views/pages/about/index.blade.php
+++ app/views/pages/about/index.blade.php
@@ -8,52 +8,92 @@
 
 	<p>We believe that the true joy for any student of the game comes from the passion to learn more about the subject, the commitment to practice and develop their golf game and the challenge to continually improve their golf and themselves. Make a commitment to yourself to get better and visit our Academy to take your game inside the ropes.</p>
 
-	<div class="row-fluid">
-		<div class="span4">
-			<div class="well">
-				<h3 class="primary visible-desktop">Private Instruction</h3>
-				<h3 class="primary visible-tablet">Private Lessons</h3>
-				<p>Book a private instruction session or playing lesson at Mill Creek Golf Club with a Brian Jacobs Golf Instructor and start improving today.</p>
-				
-				<div class="btn-group hidden-phone">
-					<a href="{{ URL::to_route('private') }}" class="btn btn-primary">More Info</a>
-					<a href="{{ URL::to_route('booking') }}" class="btn btn-primary">Book Now</a>
-				</div>
+	<div class="hidden-tablet">
+		<div class="row-fluid">
+			<div class="span4">
+				<div class="well">
+					<h3 class="primary visible-desktop">Private Instruction</h3>
+					<h3 class="primary visible-tablet">Private Lessons</h3>
+					<p>Book a private instruction session or playing lesson at Mill Creek Golf Club with a Brian Jacobs Golf Instructor and start improving today.</p>
+					
+					<div class="btn-group hidden-phone">
+						<a href="{{ URL::to_route('private') }}" class="btn btn-primary">More Info</a>
+						<a href="{{ URL::to_route('booking') }}" class="btn btn-primary">Book Now</a>
+					</div>
 
-				<div class="visible-phone">
-					<a href="{{ URL::to_route('private') }}" class="btn btn-primary btn-block btn-large">More Info</a>
+					<div class="visible-phone">
+						<a href="{{ URL::to_route('private') }}" class="btn btn-primary btn-block btn-large">More Info</a>
+						<a href="{{ URL::to_route('booking') }}" class="btn btn-primary btn-block btn-large">Book Now</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="span4">
+				<div class="well">
+					<h3 class="primary">Golf Schools</h3>
+					<p>Participate in one of our many golf schools where you'll be given the tools necessary to take your golf game to the next level.</p>
+					
+					<div class="btn-group hidden-phone">
+						<a href="{{ URL::to_route('schools') }}" class="btn btn-primary">More Info</a>
+						<a href="{{ URL::to_route('contact', array('schools')) }}" class="btn btn-primary">Book Now</a>
+					</div>
+
+					<div class="visible-phone">
+						<a href="{{ URL::to_route('schools') }}" class="btn btn-primary btn-block btn-large">More Info</a>
+						<a href="{{ URL::to_route('contact', array('schools')) }}" class="btn btn-primary btn-block btn-large">Book Now</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="span4">
+				<div class="well">
+					<h3 class="primary">Spring Golf Trip</h3>
+					<p>Join us for a getaway to Amelia Island Plantation complete with amazing views from a seaside villa and 3 days of championship golf.</p>
+					
+					<div class="btn-group hidden-phone">
+						<a href="{{ URL::to_route('event-amelia-island') }}" class="btn btn-primary">More Info</a>
+					</div>
+
+					<div class="visible-phone">
+						<a href="{{ URL::to_route('event-amelia-island') }}" class="btn btn-primary btn-block btn-large">More Info</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="visible-tablet">
+		<div class="row-fluid">
+			<div class="span12">
+				<div class="well">
+					<h3 class="primary visible-desktop">Private Instruction</h3>
+					<h3 class="primary visible-tablet">Private Lessons</h3>
+					<p>Book a private instruction session or playing lesson at Mill Creek Golf Club with a Brian Jacobs Golf Instructor and start improving today.</p>
+
+					<a href="{{ URL::to_route('private') }}" class="btn btn-primary btn-block btn-large">More Info</a><br>
 					<a href="{{ URL::to_route('booking') }}" class="btn btn-primary btn-block btn-large">Book Now</a>
 				</div>
 			</div>
 		</div>
 
-		<div class="span4">
-			<div class="well">
-				<h3 class="primary">Golf Schools</h3>
-				<p>Participate in one of our many golf schools where you'll be given the tools necessary to take your golf game to the next level.</p>
-				
-				<div class="btn-group hidden-phone">
-					<a href="{{ URL::to_route('schools') }}" class="btn btn-primary">More Info</a>
-					<a href="{{ URL::to_route('contact', array('schools')) }}" class="btn btn-primary">Book Now</a>
-				</div>
-
-				<div class="visible-phone">
-					<a href="{{ URL::to_route('schools') }}" class="btn btn-primary btn-block btn-large">More Info</a>
+		<div class="row-fluid">
+			<div class="span12">
+				<div class="well">
+					<h3 class="primary">Golf Schools</h3>
+					<p>Participate in one of our many golf schools where you'll be given the tools necessary to take your golf game to the next level.</p>
+					
+					<a href="{{ URL::to_route('schools') }}" class="btn btn-primary btn-block btn-large">More Info</a><br>
 					<a href="{{ URL::to_route('contact', array('schools')) }}" class="btn btn-primary btn-block btn-large">Book Now</a>
 				</div>
 			</div>
 		</div>
 
-		<div class="span4">
-			<div class="well">
-				<h3 class="primary">Spring Golf Trip</h3>
-				<p>Join us for a getaway to Amelia Island Plantation complete with amazing views from a seaside villa and 3 days of championship golf.</p>
-				
-				<div class="btn-group hidden-phone">
-					<a href="{{ URL::to_route('event-amelia-island') }}" class="btn btn-primary">More Info</a>
-				</div>
-
-				<div class="visible-phone">
+		<div class="row-fluid">
+			<div class="span12">
+				<div class="well">
+					<h3 class="primary">Spring Golf Trip</h3>
+					<p>Join us for a getaway to Amelia Island Plantation complete with amazing views from a seaside villa and 3 days of championship golf.</p>
+					
 					<a href="{{ URL::to_route('event-amelia-island') }}" class="btn btn-primary btn-block btn-large">More Info</a>
 				</div>
 			</div>
