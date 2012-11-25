342bbca73ede30a8be217c7e58322025d946d3ba
David VanScott <david.vanscott@gmail.com>
2012-11-25 01:04:06 -0500

    Add a link to Google Maps in the footer

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -6,9 +6,11 @@
 				
 				<p>A 23-acre practice center at Mill Creek Golf Club accommodates all levels of golfers serious about their game. The bent grass driving range has over 400 yards of hitting area with six target greens and the short game practice area includes three separate greens for pitching/chipping, putting and bunker play.</p>
 				
-				128 Cedars Avenue<br>
+				<p>128 Cedars Avenue<br>
 				Churchville, NY 14428<br>
-				(585) 889-4110
+				(585) 889-4110</p>
+
+				<p><a href="http://maps.google.com/?q=128 Cedars Ave Churchville, NY 14428" target="_blank"><span class="map-pin"></span>View in Google Maps</a></p>
 			</div>
 		</div>
 		
@@ -48,9 +50,11 @@
 				
 				<p>A 23-acre practice center at Mill Creek Golf Club accommodates all levels of golfers serious about their game. The bent grass driving range has over 400 yards of hitting area with six target greens and the short game practice area includes three separate greens for pitching/chipping, putting and bunker play.</p>
 				
-				128 Cedars Avenue<br>
+				<p>128 Cedars Avenue<br>
 				Churchville, NY 14428<br>
-				(585) 889-4110
+				(585) 889-4110</p>
+
+				<p><a href="http://maps.google.com/?q=128 Cedars Ave Churchville, NY 14428" target="_blank"><span class="map-pin"></span>View in Google Maps</a></p>
 			</div>
 		</div>
 	</div>
