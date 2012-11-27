c3878fbe5605d19bdf54dfaa5410fd571f54f035
David VanScott <david.vanscott@gmail.com>
2012-11-27 08:24:52 -0500

    Add additional content for Amelia Island page

--- app/views/pages/events/amelia-island.blade.php
+++ app/views/pages/events/amelia-island.blade.php
@@ -9,6 +9,18 @@
 
 	<p>Between April 1st and 6th of 2013, Brian Jacobs Golf will be taking a small group of golfers down to sunny Florida and the exquisite golfer's paradise of the Omni Amelia Island Plantation. Featuring 36 holes of championship golf nestled between Spanish moss-covered oaks and the Atlantic Ocean, the resort boasts designs by Bobby Weed and the legendary Pete Dye that will challenge golfers of all levels.</p>
 
+	<p>Each morning will begin with a clinic on the range with a Brian Jacobs Instructor before heading out to the course for 18 holes on the spectacular courses at Amelia Island. (Replay rounds will be available for $24.) After your round, you'll have the opportunity to book lessons with a Brian Jacobs Instructor if you need to work on different areas of your game.</p>
+
+	<p>For your stay at the resort, there are several housing options:</p>
+
+	<ul>
+		<li>3 Bedroom Resort View Villa (Quad) living will be arranged for entire group unless otherwise indicated. (3 bedrooms housing, 4 people per Villa-Resort Villa contains 1 king bed, 1 queen and 2 twins plus a kitchen/living area-2 people will need to share the room with twin beds.)</li>
+		<li>You may request 3 Bedroom Resort View Villa for 3 people for a fee of $35 additional per person/per night.</li>
+		<li>You may request a 2 bedroom Resort View Villa for 2 people for a fee of $75 additional per person/per night.</li>
+	</ul>
+
+	<p>Please <a href="{{ URL::to_route('contact', array('general')) }}">contact us</a> and make your request if 3 Bedroom Resort View Villa (Quad) Living arrangement is unacceptable.</p>
+
 	<p>Space is limited, so make sure you reserve your spot today with your $200 deposit!</p>
 
 	<h3>$995*</h3>
@@ -39,5 +51,5 @@
 		</div>
 	</div>
 
-	<p class="muted">* Airfare not included</p>
+	<p class="muted">* Airfare/meals not included</p>
 @endsection
