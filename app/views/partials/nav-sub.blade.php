6a6d4502acf4081f5bef9bdf4e5acd4d323f535f
David VanScott <david.vanscott@gmail.com>
2013-02-15 11:33:37 -0500

    Add some of the events to the sub nav

--- app/views/partials/nav-sub.blade.php
+++ app/views/partials/nav-sub.blade.php
@@ -36,6 +36,8 @@
 	@elseif (URI::is('event/*'))
 		<ul>
 			<li class="{{ URI::is('event/amelia-island') ? 'active' : '' }}"><a href="{{ URL::to_route('event-amelia-island') }}">Amelia Island</a><div class="arrow"></div></li>
+			<li class="{{ URI::is('event/storm-the-course') ? 'active' : '' }}"><a href="{{ URL::to_route('event-storm') }}">Storm the Course</a><div class="arrow"></div></li>
+			<li class="{{ URI::is('event/4-elements') ? 'active' : '' }}"><a href="{{ URL::to_route('event-elements') }}">The 4 Elements</a><div class="arrow"></div></li>
 		</ul>
 	@endif
 </nav>
@@ -73,6 +75,8 @@
 	@elseif (URI::is('event/*'))
 		<ul class="nav nav-list">
 			<li class="{{ URI::is('event/amelia-island') ? 'active' : '' }}"><a href="{{ URL::to_route('event-amelia-island') }}">Amelia Island</a></li>
+			<li class="{{ URI::is('event/storm-the-course') ? 'active' : '' }}"><a href="{{ URL::to_route('event-storm') }}">Storm the Course</a><div class="arrow"></div></li>
+			<li class="{{ URI::is('event/4-elements') ? 'active' : '' }}"><a href="{{ URL::to_route('event-elements') }}">The 4 Elements</a><div class="arrow"></div></li>
 		</ul>
 	@endif
 </div>
